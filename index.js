a4587d5e53c41a289dd359d47ed081f5d8fffc08
Kyoko <kyokoconnect@gmail.com>
2021-03-31 01:11:06 -0400

    complete lab test

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'current user';
+let welcomeMessage = 'Welcome to Flatbook, current user!';
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, CURRENT USER!';
+let shortGreeting = 'Welcome, ' + '${firstInitial}' + '!';
